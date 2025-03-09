d9512eb0ffad5547a8f27f0bcbf44010e76b36c8
MO-GBR <mohameedgabr7@gmail.com>
2025-03-09 16:53:05 +0200

    eslint rules edit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,19 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
+// Old Config
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+// New Config
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "next"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    }
+  })
 ];
 
 export default eslintConfig;
