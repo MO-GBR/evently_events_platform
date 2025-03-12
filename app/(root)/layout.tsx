7485f78ec5791f47e1df9cc50a541980ddf2f493
Mohamed Gabr <70921709+MO-GBR@users.noreply.github.com>
2025-03-12 05:03:10 +0200

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children } : { children: React.ReactNode })
     return (
         <div>
             <Header />
-            <main className="mt-20">{children}</main>
+            <main className="mt-22">{children}</main>
             <Footer />
         </div>
     )
